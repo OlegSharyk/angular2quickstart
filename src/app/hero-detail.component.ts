b1349b406eb362f286a584374479d46fe96ab793
Oleg Sharik <support@z-index.com.ua>
2017-05-14 18:37:06 +0300

    second commit from home

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,7 +8,7 @@ import { Hero } from './hero';
       <h2>{{hero.name}} details!</h2>
       <div><label>id: </label>{{hero.id}}</div>
       <div>
-        <label>name: </label>
+        <label>Name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
     </div>
